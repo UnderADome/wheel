5158f52c5931817c90ac861c504210a306fb0ae6
litao <litaozuel@outlook.com>
2019-03-31 22:09:54 +0800

    merge branch in laptop and desktop

--- src/main/java/orz/reinvent/wheel/WheelApplication.java
+++ src/main/java/orz/reinvent/wheel/WheelApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WheelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WheelApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WheelApplication.class, args);
+    }
 
 }
